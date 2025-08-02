ba3f92a866e976f5be60e29448fe9f03a7837e93
Ayush saxena <155671320+aayushgi@users.noreply.github.com>
2025-08-02 12:22:42 +0530

    Add files via upload

---
+++ datatype.java
@@ -0,0 +1,21 @@
+public class datatype {
+    public static void main(String[] args) {
+        boolean flag = true;
+        int a = 100;
+        short s = 1200;
+        byte b = 12;
+        long l = 5533644;
+        float f = 800.5655f;
+        double d = 800.5655;
+        char ch = 'A';
+
+        System.out.println("the boolean variable is " + flag);
+        System.out.println("the integer variable is " + a);
+        System.out.println("the short variable is " + s);
+        System.out.println("the byte variable is " + b);
+        System.out.println("the long variable is " + l);
+        System.out.println("the float variable is " + f);
+        System.out.println("the double variable is " + d);
+        System.out.println("the character variable is " + ch);
+    }
+}
