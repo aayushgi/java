9015d0bbeae0b198a948d2dbf6cd635b12d19c0c
Ayush saxena <155671320+aayushgi@users.noreply.github.com>
2025-08-02 11:50:10 +0530

    Add files via upload

---
+++ basic.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;;
+class basic {
+
+    public static void main(String[] args) {
+        // Scanner sc=new Scanner(System.in);
+
+        // System.out.println("enter something");
+        // String str=sc.nextLine();
+        // System.out.println("u entered"+str);
+        int weight = 46;
+        System.out.println(weight);
+       }
+}
