fe0cfe8a62362a32036cb391562af3ca4a7cc9c5
Ayush saxena <155671320+aayushgi@users.noreply.github.com>
2025-08-04 13:09:54 +0530

    Add files via upload

--- arithmaticoperator.java
+++ arithmaticoperator.java
@@ -2,10 +2,19 @@ public class arithmaticoperator {
     public static void main(String[] args) {
         int a = 100,  b= 200;
 
-        System.out.println(a+b);
-        System.out.println(a*b);
-        System.out.println(a%b);
-        System.out.println(a/b);
+        // System.out.println(a+b);
+        // System.out.println(a*b);
+        // System.out.println(a%b);
+        // System.out.println(a/b);
+      
 
+        // relational operator
+        
+        System.out.println(a==b);
+        System.out.println(a!=b);
+        System.out.println(a>=b);
+        System.out.println(a<=b);
+        System.out.println(a>b);
+        System.out.println(a<b);
     }
 }
