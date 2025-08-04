e826c28cb558c5b1da23df07e30f89442b6380af
Ayush saxena <155671320+aayushgi@users.noreply.github.com>
2025-08-04 13:11:14 +0530

    Add files via upload

---
+++ assignementoperator.java
@@ -0,0 +1,24 @@
+public class assignementoperator {
+     public static void main(String[] args) {
+        int p = 10;
+        int q;
+        // =
+        q = p;
+         System.out.println(q);  
+         //10
+
+         //+=
+         p += q; //p=p+q
+         System.out.println(p);
+
+         //-=
+
+         p -= q;
+         System.out.println(p);
+
+         //*=
+        p *= q;
+        System.out.println(p);
+
+}
+}
