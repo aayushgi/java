1be812e0f02422a18225dcc03266f8a8de6d3818
Ayush saxena <155671320+aayushgi@users.noreply.github.com>
2025-08-05 21:26:13 +0530

    Add files via upload

---
+++ loops/quest1.java
@@ -0,0 +1,14 @@
+   //print frist multiple of 5 which is also a mutiple of 7//
+
+public class quest1 { 
+    public static void main(String[] args) {
+        int num = 1;
+        while(true){
+            if(num % 5 == 0 && num % 7 == 0){
+                System.out.println("found ans " +num);
+                break;
+        }
+        num++;
+    }
+}
+}
