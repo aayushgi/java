a0efaf1a86b65e7dfe39557029ba25e8bbace4b6
Ayush saxena <155671320+aayushgi@users.noreply.github.com>
2025-08-10 22:35:10 +0530

    Add files via upload

---
+++ Functions/function.java
@@ -0,0 +1,13 @@
+public class function {
+    public static void printHellowWord() {
+        System.out.println("hellow word");
+        System.out.println("hellow word");
+        System.out.println("hellow word");
+        System.out.println("hellow word");
+        System.out.println("hellow word");
+        System.out.println("hellow word");
+    }
+    public static void main(String[] args) {
+        printHellowWord();
+    }
+}
