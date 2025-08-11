9cadef904956ff24743a6e789341206e18ba6e3d
Ayush saxena <155671320+aayushgi@users.noreply.github.com>
2025-08-11 22:13:42 +0530

    Add files via upload

---
+++ Functions/product.java
@@ -0,0 +1,19 @@
+public class product {
+    public static int multiply(int a, int b) {
+
+        int product = a*b;
+        return product;
+    }
+    public static void main(String[] args) {
+        int a=2;
+        int b=3;
+        int prod = multiply(a, b);
+        System.out.println( "The product of " +prod);
+
+        multiply(10, 20);
+        System.out.println("a*b=" +prod);
+
+    }
+    
+    
+}
