3748a903d02572f65abd26e4820f15a7835e6bea
Ayush saxena <155671320+aayushgi@users.noreply.github.com>
2025-08-16 20:28:09 +0530

    Add files via upload

---
+++ Functions/binarytodecimal.java
@@ -0,0 +1,22 @@
+public class binarytodecimal {
+    public static void binTodec(int binNum) {
+         int myNumber = binNum;
+        int pow=0;
+        int decNum=0;
+
+        while (binNum>0) {
+            int lastDigit=binNum%10;
+            decNum = decNum + (lastDigit*(int)Math.pow(2, pow));
+
+            pow++;
+            binNum = binNum/10;
+            
+        }
+        System.out.println("decimal of" +binNum + " = " +decNum);
+    }
+
+    public static void main(String[] args) {
+        binTodec(10001);
+        
+    }
+}
