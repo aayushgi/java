dd65bbdbcdb56cb0606bac9e6e2845604163b127
Ayush saxena <155671320+aayushgi@users.noreply.github.com>
2025-09-03 11:40:45 +0530

    Add files via upload

---
+++ Array/ArrayInput.java
@@ -0,0 +1,36 @@
+import java.util.Scanner;
+
+public class ArrayInput {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter the size of the array: ");
+        int size = sc.nextInt();
+
+        int[] array = new int[size];
+
+        System.out.println("Enter the elements of the array:");
+        for (int i = 0; i < size; i++) {
+            array[i] = sc.nextInt();
+        }
+
+        // Bubble sort
+        for (int i = 0; i < size - 1; i++) {
+            for (int j = 0; j < size - i - 1; j++) {
+                if (array[j] > array[j + 1]) {
+                    // swap
+                    int temp = array[j];
+                    array[j] = array[j + 1];
+                    array[j + 1] = temp;
+                }
+            }
+        }
+
+        System.out.println("The sorted array elements are:");
+        for (int i = 0; i < size; i++) {
+            System.out.print(array[i] + " ");
+        }
+
+        sc.close();
+    }
+}
